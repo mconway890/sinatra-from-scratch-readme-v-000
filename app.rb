bb961467a6be3566e92faedfde062ab6771b5da8
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2017-12-13 17:38:53 +0000

    edit app.rb to run in browser

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
-class App < Sinatra::Base
+# class App < Sinatra::Base
 
   get '/' do 
     "Hello, world!"
   end
 
-end
+# end
+
+# "Hello, world!" would not appear in browser while in class App. Once commented out
+# it did load in browser. Not far along enough in Sinatra to know why. Noticed when forking
+# and cloning lab that there is no config file or 'run App' in any of the loaded files. 
